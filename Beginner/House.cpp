9fd30435664dad8190d20b8e4e9203e723cd57c8
maksymka <bondarmaksymwork@gmail.com>
2016-02-07 03:13:25 +0200

    House

---
+++ Beginner/House.cpp
@@ -0,0 +1,21 @@
+// House.cpp: определяет точку входа для консольного приложения.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "\t\tHouse:\n\n";
+	std::cout << "\t\t\/\\\n";
+	std::cout << "\t       \/  \\\n"
+		<< "\t      \/    \\\n" 
+		<< "\t     \/------\\\n"
+		<< "\t    \/ |    | \\\n"
+		<< "\t      |    |\n"
+		<< "\t      |    |\n"
+		<< "\t      |____|\n";
+	system("pause>>void");
+	return 0;
+}
+
